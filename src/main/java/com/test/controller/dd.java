bb4b6a742006da19bfdb35f349d6e923df9580f5
poppetman <poppetman@outlok.com>
2018-07-21 21:29:02 +0800

    SpringBoot整合Freemarker

---
+++ src/main/java/com/test/controller/dd.java
@@ -0,0 +1,14 @@
+package com.test.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+
+/**
+ * @author huangsongyang
+ * @date 2018/7/21 21:26
+ */
+public class dd {
+
+}
+
+
+
